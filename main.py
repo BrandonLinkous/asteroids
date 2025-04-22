19e6ec863a855510a501edede0e62aee90164725
Logician <viishusgaming@gmail.com>
2025-04-21 20:22:30 -0400

    added ship and wasd functionality

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
-        screen.fill("black")
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        updatable.update(dt)
+        
+        for items in drawable:
+            items.draw(screen)
+        
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
